8c3726f2fe95adacf3995f8b02ad47fbce73aa1a
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-03-13 16:32:57 +0530

    Time: 3 ms (51.39%) | Memory: 7.1 MB (87.40%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     int pivotInteger(int n) {
+        int left = 1, right = n;
         int total = n*(n+1)/2;
 
-        int pivot = sqrt(total);
+        while(left <= right){
+            int pivot = left + (right - left)/2;
 
-        return (pivot*pivot == total)? pivot:-1;
+            if(pivot*pivot == total){
+                return pivot;
+            }else if(pivot*pivot > total){
+                right = pivot - 1;
+            }else{
+                left = pivot + 1;
+            }
+        }
+        return -1;
     }
 };
