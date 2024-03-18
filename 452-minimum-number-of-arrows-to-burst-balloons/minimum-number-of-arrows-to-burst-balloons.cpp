8931fd8781a6e851518f14ca0f9137cc6c1dd55a
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-03-18 16:54:13 +0530

    Time: 236 ms (93.50%) | Memory: 93.1 MB (95.79%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -4,21 +4,21 @@ public:
         int n = points.size();
         sort(points.begin(), points.end());
         vector<int>prev = points[0];
-        int cnt=1;
+        int cnt = 1;
 
         for(int i=1; i<n; i++){
-            int currStart = points[i][0];
-            int currEnd = points[i][1];
+            int curStart = points[i][0];
+            int curEnd = points[i][1];
 
             int prevStart = prev[0];
             int prevEnd = prev[1];
 
-            if(currStart > prevEnd){
+            if(curStart > prevEnd){
                 cnt++;
                 prev = points[i];
             }else{
-                prev[0] = max(prevStart, currStart);
-                prev[1] = min(currEnd, prevEnd);
+                prev[0] = max(prevStart, curStart);
+                prev[1] = min(curEnd, prevEnd);
             }
         }
         return cnt;
