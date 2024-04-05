3ebd615e2e6704ac6409a0915b7d0594d0816764
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-04-05 23:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (11.17%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char>st;;
+        int n = s.size();
+
+        for(int i=0; i<n; i++){
+            if(st.empty()) st.push(s[i]);
+            else if(abs(st.top()-s[i]) == 32) st.pop();
+            else st.push(s[i]);
+        }
+
+        string ans = "";
+        while(!st.empty()){
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
