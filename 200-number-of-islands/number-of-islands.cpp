53efa7ce40092ca844ca6c34754f5203ff0eadc8
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-04-19 16:50:13 +0530

    Time: 108 ms (5.18%) | Memory: 29.4 MB (8.90%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int n, m;
     vector<vector<int>>directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<int>>&vis){
-        if(i<0 or i>=n or j<0 or j>=m or grid[i][j] == '0' or vis[i][j] == 1){
+    void dfs(int i, int j, vector<vector<char>>& grid){
+        if(i<0 or i>=n or j<0 or j>=m or grid[i][j] == '0' or grid[i][j] == '$'){
             return;
         }
-        vis[i][j] = 1;
-
+        // vis[i][j] = 1;
+        grid[i][j] = '$';
         for(auto dir: directions){
             int nrow = i + dir[0];
             int ncol = j + dir[1];
 
-            dfs(nrow, ncol, grid, vis);
+            dfs(nrow, ncol, grid);
         }
     }
 
@@ -20,18 +20,15 @@ public:
         n = grid.size();
         m = grid[0].size();
         int cnt= 0;
-        vector<vector<int>>vis(n, vector<int>(m, 0));
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == '1'){
-                    if(vis[i][j] == 0){
-                        cnt++;
-                        dfs(i, j, grid, vis);
-                    }
+                    cnt++;
+                    dfs(i, j, grid);
                 }
             }
-        }  
+        }
         return cnt;  
     }
 };
