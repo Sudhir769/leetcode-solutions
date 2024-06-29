e0ca8759e17a64e107a99c3ba9479fe3e5907851
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-06-29 13:05:48 +0530

    Time: 91 ms (94.95%) | Memory: 63.8 MB (89.79%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,65 +1,60 @@
-// class Solution {
-// public:
-//     void dfs(int node, int par, vector<int> &vis, vector<vector<int>>&parent, vector<int>adj[]){
-//         vis[node] = 1;
-        
-//         for(auto it:adj[node]){
-//             if(!vis[it]){
-//                 parent[node].push_back(par);
-//                 dfs(it, node, vis, parent, adj);
-//             }
-//         }
-//     }
-
-//     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-//         vector<int>adj[n];
-//         for(auto edge:edges){
-//             adj[edge[0]].push_back(edge[1]);
-//         }
-
-//         vector<vector<int>>parent(n);
-
-//         for(int i=0; i<n; i++){
-//             vector<int>vis(n, 0);
-//             dfs(i, i, vis, parent, adj);
-//         }
-//         for(int i=0; i<n; i++){
-//             sort(parent[i].begin(), parent[i].end());
-//         }
-//         return parent;
-//     }
-// };
-
 class Solution {
 public:
-    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> res(n);
-        vector<vector<int>> graph(n);
+    void dfs(int node, int par, vector<vector<int>>&parent, vector<int>adj[]){
         
-        for (const auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            vector<bool> visit(n, false);
-            dfs(graph, i, i, res, visit);
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            sort(res[i].begin(), res[i].end());
-        }
-        
-        return res;
-    }
-
-private:
-    void dfs(vector<vector<int>>& graph, int parent, int curr, vector<vector<int>>& res, vector<bool>& visit) {
-        visit[curr] = true;
-        for (int dest : graph[curr]) {
-            if (!visit[dest]) {
-                res[dest].push_back(parent);
-                dfs(graph, parent, dest, res, visit);
+        for(auto it:adj[node]){
+            if(parent[it].empty() or parent[it].back() != par){
+                parent[it].push_back(par);
+                dfs(it, par, parent, adj);
             }
         }
     }
+
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<int>adj[n];
+        for(auto edge:edges){
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        vector<vector<int>>parent(n);
+
+        for(int i=0; i<n; i++){
+            dfs(i, i, parent, adj);
+        }
+        return parent;
+    }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+//         vector<vector<int>> res(n);
+//         vector<vector<int>> graph(n);
+        
+//         for (const auto& edge : edges) {
+//             graph[edge[0]].push_back(edge[1]);
+//         }
+        
+//         for (int i = 0; i < n; ++i) {
+//             vector<bool> visit(n, false);
+//             dfs(graph, i, i, res, visit);
+//         }
+        
+//         // for (int i = 0; i < n; ++i) {
+//         //     sort(res[i].begin(), res[i].end());
+//         // }
+        
+//         return res;
+//     }
+
+// private:
+//     void dfs(vector<vector<int>>& graph, int parent, int curr, vector<vector<int>>& res, vector<bool>& visit) {
+//         visit[curr] = true;
+//         for (int dest : graph[curr]) {
+//             if (!visit[dest]) {
+//                 res[dest].push_back(parent);
+//                 dfs(graph, parent, dest, res, visit);
+//             }
+//         }
+//     }
+// };
