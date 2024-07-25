f4d6e9070e085ed67114e56d1bdac7a0fb3780af
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-07-25 06:25:16 +0530

    Time: 130 ms (61.91%) | Memory: 78.7 MB (49.24%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums;
+        int n = nums.size();
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for(int i=0; i<n; i++){
+            pq.push(nums[i]);
+        }
+
+        vector<int>ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
     }
 };
