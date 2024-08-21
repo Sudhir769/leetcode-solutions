e7986ce9e7bf5907849c56c68e3f222b3185825a
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-08-21 20:22:30 +0530

    Time: 15 ms (77.42%) | Memory: 11.4 MB (26.11%) - LeetSync

---
+++ 664-strange-printer/strange-printer.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>>dp;
+    int solve(int l, int r, string &s){
+        if(l == r) return 1;
+        if(l > r) return 0;
+
+        if(dp[l][r] != -1) return dp[l][r];
+
+        int i = l+1;
+        while(i <= r and s[l] == s[i]){
+            i++;
+        }
+        if(i == r+1) return 1;
+
+        int basic = 1 + solve(i, r, s);
+
+        int greedy = INT_MAX;
+        for(int j=i; j<=r; j++){
+            if(s[j] == s[l]){
+                int ans = solve(i, j-1, s) + solve(j, r, s);
+                greedy = min(greedy, ans);
+            }
+        }
+
+        return dp[l][r] = min(basic, greedy);
+    }
+    int strangePrinter(string s) {
+        n = s.length();
+        dp.resize(n, vector<int>(n+1, -1));
+        return solve(0, n-1, s);
+    }
+};
