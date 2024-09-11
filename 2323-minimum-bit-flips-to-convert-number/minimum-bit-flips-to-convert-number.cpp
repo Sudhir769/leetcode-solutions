f46e06de41be00e745d97a0a65f843c6eaf5c02d
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-09-11 13:51:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (44.87%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start^goal);
+        bitset<32> startBit(start), goalBit(goal);
+        int ans = 0;
+        for(int i=0; i<32; i++){
+            if(startBit[i] != goalBit[i]) ans++;
+        }
+        return ans;
     }
 };
