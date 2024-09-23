7badeb062a176e2353dd531dccc60c1f0e253b54
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-09-23 14:47:42 +0530

    Time: 126 ms (56.59%) | Memory: 86.8 MB (52.70%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(int i, int end, string &s, unordered_set<string> &st){
-        if(i >= end){
-            return 0;
-        }
-        if(dp[i] != -1) return dp[i];
-        int result = 1 + solve(i+1, end, s, st);
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.length();
+        unordered_set<string> st(dictionary.begin(), dictionary.end());
 
-        for(int ind = i; ind < end; ind++){
-            if(st.count(s.substr(i, ind-i+1))){
-                result = min(result, solve(ind+1, end, s, st));
+        vector<int> dp(n+1, 0);
+
+        for(int i=n-1; i>=0; i--){
+            dp[i] = 1 + dp[i+1];
+
+            for(int j=i; j<n; j++){
+                string str = s.substr(i, j-i+1);
+                if(st.count(str)){
+                    dp[i] = min(dp[i], dp[j+1]);
+                }
             }
         }
-        return dp[i] = result;
-    }
-
-    int minExtraChar(string s, vector<string>& dictionary) {
-        int end = s.length();
-        unordered_set<string>st(dictionary.begin(), dictionary.end());
-        dp.resize(end+1, -1);
-        return solve(0, end, s, st);
+        return dp[0];
     }
 };
