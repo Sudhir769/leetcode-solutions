cf9850d1619315fadc956a06ec07033e083c12d9
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-10-05 19:14:30 +0530

    Time: 2472 ms (5.06%) | Memory: 123.6 MB (5.57%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        sort(s1.begin(), s1.end());
+
+        for(int i=0; i<m; i++){
+            string substr = s2.substr(i, n);
+            sort(substr.begin(), substr.end());
+
+            if(s1 == substr)return true;
+        }
+        return false;
+    }
+};
