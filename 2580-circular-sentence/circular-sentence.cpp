b7cf1f2e9788148c750fd46ddef9917b30acae85
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-11-02 21:48:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (59.78%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -3,6 +3,8 @@ public:
     bool isCircularSentence(string s) {
         int n = s.length();
 
+        if(s[0] != s.back()) return false;
+
         for(int i=0; i<n; i++){
             if(s[i] == ' '){
                 if(s[i-1] != s[i+1]){
@@ -10,6 +12,6 @@ public:
                 }
             }
         }
-        return s[0] == s[n-1];
+        return true;
     }
 };
