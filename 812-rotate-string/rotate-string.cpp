8bded72cb4077cf980a39ba229238562400bd286
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-11-03 18:05:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (55.81%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,16 +3,14 @@ public:
     bool rotateString(string s, string goal) {
         int n = s.length();
         int m = goal.length();
+
+        s = s + s;
         
         if(m != n){
             return false;
         }
-
-        for(int i=0; i<n; i++){
-            rotate(s.begin(), s.begin()+1, s.end());
-            if(s.find(goal) != string::npos){
-                return true;
-            }
+        if(s.find(goal) != string::npos){
+            return true;
         }
         return false;
     }
