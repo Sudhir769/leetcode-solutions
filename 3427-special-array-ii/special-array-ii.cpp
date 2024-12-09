3cc316d7a59bda3d439e86d3affe6474fd121f42
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-12-09 20:41:49 +0530

    Time: 4 ms (87.50%) | Memory: 126.2 MB (76.85%) - LeetSync

---
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool bSearch(vector<int> &vi, int start, int end){
+        int l=0;
+        int r=vi.size()-1;
+
+        while(l <= r){
+            int mid = l + (r-l)/2;
+
+            if(vi[mid] < start){
+                l = mid+1;
+            }else if(vi[mid] > end){
+                r = mid-1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int m = queries.size();
+
+        vector<int> vi;
+        for(int i=1; i<n; i++){
+            if(nums[i] % 2 == nums[i-1] % 2){
+                vi.push_back(i);
+            }
+        }
+        vector<bool> result(m, false);
+        for(int i=0; i<m; i++){
+            int start = queries[i][0];
+            int end = queries[i][1];
+
+            bool violatingIndex = bSearch(vi, start+1, end);
+            
+            if(violatingIndex == false){
+                result[i] = true;
+            }
+        }
+        return result;
+    }
+};
