15536002b74f96903e133380114c9ca6542f84a9
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-12-11 21:05:12 +0530

    Time: 47 ms (71.90%) | Memory: 105 MB (67.70%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    int bSearch(vector<int>& nums, int start, int end, int target){
-        int result = 0;
-        while(start <= end){
-            int mid = start + (end-start)/2;
-
-            if(nums[mid] <= target){
-                result = mid;
-                start = mid+1;
-            }else{
-                end = mid-1;
-            }
-        }
-        return result;
-    }
     int maximumBeauty(vector<int>& nums, int k) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
         int result = 0;
-        for(int i=0; i<n; i++){
-            int x = nums[i];
-            int y = x + 2*k;
-
-            int j = bSearch(nums, i+1, n-1, y);
-            result = max(result, j-i+1);
+        int i=0, j=0;
+        while(i < n){
+            while(j < n and nums[j] <= nums[i] + 2*k){
+                j++;
+            }
+            result = max(result, j-i);
+            i++;
         }
         return result;
     }
