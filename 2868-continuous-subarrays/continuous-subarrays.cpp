c9022bb45b5382b301a6058073828e2ca1509540
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-12-14 20:16:20 +0530

    Time: 163 ms (47.01%) | Memory: 129.6 MB (39.50%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.cpp
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
+    #define P pair<int, int>
     long long continuousSubarrays(vector<int>& nums) {
         int n = nums.size();
+
+        priority_queue<P, vector<P>> maxpq;
+        priority_queue<P, vector<P>, greater<P>> minpq;
 
         int i=0, j=0;
         long long cnt = 0;
 
-        map<int, int> mp;
         while(j < n){
-            mp[nums[j]]++;
 
-            while(mp.rbegin()->first - mp.begin()->first > 2){
-                mp[nums[i]]--;
-                if(mp[nums[i]] == 0){
-                    mp.erase(nums[i]);
-                }
+            minpq.push({nums[j], j});
+            maxpq.push({nums[j], j});
+
+            while(maxpq.top().first - minpq.top().first > 2){
                 i++;
+                
+                while(!maxpq.empty() and maxpq.top().second < i) maxpq.pop();
+                while(!minpq.empty() and minpq.top().second < i) minpq.pop();
             }
+            
             cnt += j-i+1;
             j++;
         }
+
         return cnt;
     }
 };
