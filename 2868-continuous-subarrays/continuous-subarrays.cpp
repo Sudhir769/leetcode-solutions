f16e6727cc9eae673e036fc793cf59a0ebbc05bd
Sudhir Maurya <sudhirmaurya769@gmail.com>
2024-12-14 19:46:30 +0530

    Time: 96 ms (76.95%) | Memory: 113 MB (85.43%) - LeetSync

---
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        int n = nums.size();
+
+        int i=0, j=0;
+        long long cnt = 0;
+
+        map<int, int> mp;
+        while(j < n){
+            mp[nums[j]]++;
+
+            while(mp.rbegin()->first - mp.begin()->first > 2){
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0){
+                    mp.erase(nums[i]);
+                }
+                i++;
+            }
+            cnt += j-i+1;
+            j++;
+        }
+        return cnt;
+    }
+};
