ca088426f111a1a7ba1804b2e0891b4a0cd9856b
Sudhir Maurya <sudhirmaurya769@gmail.com>
2025-02-19 14:11:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (90.21%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    vector<string> ans;
-    void solve(int n, string& temp){
+    string ans;
+    int cnt = 0;
+    void solve(int n, string& temp, int k){
         if(temp.length() == n){
-            ans.push_back(temp);
+            cnt++;
+            if(cnt == k) ans = temp;
             return;
         }
+
+        if(!ans.empty()) return;
 
         for(char ch='a'; ch<='c'; ch++){
             if(!temp.empty() and temp.back() == ch) continue;
 
             temp.push_back(ch);
-            solve(n, temp);
+            solve(n, temp, k);
             temp.pop_back();
         }
     }
     string getHappyString(int n, int k) {
         string temp = "";
-        solve(n, temp);
-        if(ans.size() < k) return "";
-        return ans[k-1];
+        solve(n, temp, k);
+        return ans;
     }
 };
