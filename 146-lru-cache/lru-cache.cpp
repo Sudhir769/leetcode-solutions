16be8b079d3f415bc3649e29d8a5b238e33be7f4
Sudhir Maurya <sudhirmaurya769@gmail.com>
2025-03-03 23:57:13 +0530

    Time: 84 ms (56.24%) | Memory: 181.9 MB (42.01%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,81 @@
+class Node{
+public:
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
+
+    Node(int key, int val){
+        this->key = key;
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+class LRUCache {
+public:
+    int capacity;
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
+    unordered_map<int, Node*> mp;
+    int length;
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        this->length = 0;
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void insertAtHead(Node* node){
+        node->next = head->next;
+        head->next = node;
+
+        node->prev = head;
+        node->next->prev = node;
+
+        mp[node->key] = node;
+        length++;
+    }
+
+    void deleteNode(Node* node){
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        mp.erase(node->key);
+        length--;
+    }
+    
+    int get(int key) {
+        if(mp.count(key)){
+            int a = mp[key]->val;
+            Node* node = mp[key];
+            deleteNode(node);
+            insertAtHead(node);
+            return a;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(mp.count(key)){
+            Node* prevNode = mp[key];
+            deleteNode(prevNode);
+            insertAtHead(prevNode);
+            prevNode->val = value;
+            return;
+        }
+
+        Node* newNode = new Node(key, value);
+        if(length >= capacity){
+            deleteNode(tail->prev);
+        }
+        insertAtHead(newNode);
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
