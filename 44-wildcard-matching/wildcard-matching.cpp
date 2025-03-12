fc94504792afaea052ffcf05e3fa55dbe1dd3432
Sudhir Maurya <sudhirmaurya769@gmail.com>
2025-03-12 21:56:50 +0530

    Time: 37 ms (31.54%) | Memory: 32 MB (5.84%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<vector<int>> dp;
     bool solve(int i, int j, string &s, string &p){
-        if(i == 0 and j == 0) return true;
-        if(j == 0 and i > 0) return false;
-        if(i == 0 and j > 0) {
-            for(int k=1; k<=j; k++){
-                if(p[k-1] != '*'){
+        if(i < 0 and j < 0) return true;
+        if(j < 0 and i >= 0) return false;
+        if(i < 0 and j >= 0) {
+            for(int k=0; k<=j; k++){
+                if(p[k] != '*'){
                     return false;
                 }
             }
@@ -15,12 +15,12 @@ public:
 
         if(dp[i][j] !=-1) return dp[i][j];
         
-        if(s[i-1] == p[j-1] | p[j-1] == '?'){
+        if(s[i] == p[j] or p[j] == '?'){
             return dp[i][j] = solve(i-1, j-1, s, p);
         }
 
-        if(p[j-1] == '*'){
-            return dp[i][j] = solve(i-1, j, s, p) | solve(i, j-1, s, p);
+        if(p[j] == '*'){
+            return dp[i][j] = solve(i, j-1, s, p) | solve(i-1, j, s, p);
         }
         return dp[i][j] = false;
     }
@@ -28,31 +28,7 @@ public:
     bool isMatch(string s, string p) {
         int n = s.length();
         int m = p.length();
-        dp.resize(n+1, vector<int>(m+1, 0));
-
-        dp[0][0] = 1;
-        for(int j=1; j<=m; j++){
-            int fl = true;
-            for(int k=1; k<=j; k++){
-                if(p[k-1] != '*'){
-                    fl = false;
-                    break;
-                }
-            }
-            dp[0][j] = fl;
-        }
-
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(s[i-1] == p[j-1] or p[j-1] == '?'){
-                    dp[i][j] = dp[i-1][j-1];
-                }else if(p[j-1] == '*'){
-                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
-                }else{
-                    dp[i][j] = false;
-                }
-            }
-        }
-        return dp[n][m];
+        dp.resize(n+1, vector<int>(m+1, -1));
+        return solve(n-1, m-1, s, p);
     }
 };
