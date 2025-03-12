e21af70eb4be6189423b651da4c133e79872e394
Sudhir Maurya <sudhirmaurya769@gmail.com>
2025-03-12 11:15:01 +0530

    Time: 682 ms (5.01%) | Memory: 382.2 MB (5.05%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(int i, int j, string word1, string word2){
+        if(i < 0) return j+1;
+        if(j < 0) return i+1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(word1[i] == word2[j]){
+            return dp[i][j] = solve(i-1, j-1, word1, word2);
+        }
+        return dp[i][j] = 1 + min(solve(i-1, j, word1, word2), solve(i, j-1, word1, word2));
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+        dp.resize(n+1, vector<int>(m+1, -1));
+
+        return solve(n-1, m-1, word1, word2);
+    }
+};
