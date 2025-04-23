1768df73209fd8bcd8bc4684676385e7434f17dc
Sudhir Maurya <sudhirmaurya769@gmail.com>
2025-04-23 22:55:52 +0530

    Time: 2 ms (49.15%) | Memory: 8.3 MB (13.56%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int digitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+    int countLargestGroup(int n) {
+        unordered_map<int, int> map;
+        int maxSize = 0;
+        
+        for (int i = 1; i <= n; ++i) {
+            int sum = digitSum(i);
+            map[sum]++;
+            maxSize = std::max(maxSize, map[sum]);
+        }
+        
+        int count = 0;
+        for (const auto& pair : map) {
+            if (pair.second == maxSize) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
